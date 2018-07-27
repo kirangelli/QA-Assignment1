caed8960063ebba5cc96e85cd9942e3f2f33f2fc
kirangelli <kirangelli31@gmail.com>
2018-07-27 13:04:29 -0300

    Making listAllCommands dynamic

--- A4/Question5/Help.java
+++ A4/Question5/Help.java
@@ -1,21 +1,22 @@
-import java.util.HashMap;
+import java.util.*;
 import java.util.Map;
 
 public class Help
 {
 	
-	public Map<String,Command> commandMap = new HashMap<String,Command>();
+	public Map<String,Command> commandMap = new LinkedHashMap<String,Command>();
 	
 	public Help()
 	{
 
 		
 		Command command = new Print();
-		commandMap.put("print",command);	
+		commandMap.put("print",command);
 		command = new Open();
 		commandMap.put("open", command);
 		command = new Close();
 		commandMap.put("close",command);
+
 		
 	}
 	public String GetHelp(String command)
@@ -29,7 +30,18 @@ public class Help
 
 	public String ListAllCommands()
 	{
+		StringBuilder listOfCommands = new StringBuilder("Commands: ");
+		List<String> keyList = new ArrayList<String>(commandMap.keySet());
+		for(int i=0;i<keyList.size();i++)
+		{
+			String key = keyList.get(i);
+			listOfCommands.append(key);
+			if(i!=keyList.size()-1)
+			{
+				listOfCommands.append(", ");
+			}
+		}
 		
-		return "Commands: print, open, close";
+		return listOfCommands.toString();
 	}
 }
